635421157f647d97781e1e7f1f74a78bbf7bc8cd
Alejandro Acevedo <dogedrive@alejandrosair2.home>
2023-10-30 03:54:39 -0400

    weather: refactored days to extracted views, moved button parameters to a text view, changed styles of the UI

--- SUI-Weather/SUI-Weather/ContentView.swift
+++ SUI-Weather/SUI-Weather/ContentView.swift
@@ -29,107 +29,34 @@ struct ContentView: View {
                         .font(.system(size: 70, weight: .medium))
                         .foregroundColor(.white)
                 }
-                Spacer()
+                .padding(.bottom, 50)
                 
-                HStack (spacing: 12) {
+                
+                HStack (spacing: 20) {
                     // day 1
-                    VStack {
-                        Text("Tue")
-                            .font(.system(size: 16, weight: .medium, design: .default))
-                            .foregroundColor(.white)
-                            .padding()
-                        VStack(spacing: 5) {
-                            Image(systemName: "sun.max.fill")
-                                .renderingMode(.original)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 60, height: 60)
-                            Text("9°")
-                                .font(.system(size: 28, weight: .bold))
-                                .foregroundColor(.white)
-                        }
-                    }
+                    WeatherDayView(dayOfWeek: "Tue", imageName: "cloud.sun.fill", temperature: 9)
                     // day 2
-                    VStack {
-                        Text("Wed")
-                            .font(.system(size: 16, weight: .medium, design: .default))
-                            .foregroundColor(.white)
-                            .padding()
-                        VStack(spacing: 5) {
-                            Image(systemName: "sun.max.fill")
-                                .renderingMode(.original)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 60, height: 60)
-                            Text("12°")
-                                .font(.system(size: 28, weight: .bold))
-                                .foregroundColor(.white)
-                        }
-                    }
+                    WeatherDayView(dayOfWeek: "Wed", imageName: "sun.max.fill", temperature: 12)
                     // day 3
-                    VStack {
-                        Text("Thu")
-                            .font(.system(size: 16, weight: .medium, design: .default))
-                            .foregroundColor(.white)
-                            .padding()
-                        VStack(spacing: 5) {
-                            Image(systemName: "wind")
-                                .renderingMode(.original)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 60, height: 60)
-                            Text("8°")
-                                .font(.system(size: 28, weight: .bold))
-                                .foregroundColor(.white)
-                        }
-                    }
+                    WeatherDayView(dayOfWeek: "Thu", imageName: "wind", temperature: 8)
                     // day 4
-                    VStack {
-                        Text("Fri")
-                            .font(.system(size: 16, weight: .medium, design: .default))
-                            .foregroundColor(.white)
-                            .padding()
-                        VStack(spacing: 5) {
-                            Image(systemName: "sun.haze.fill")
-                                .renderingMode(.original)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 60, height: 60)
-                            Text("2°")
-                                .font(.system(size: 28, weight: .bold))
-                                .foregroundColor(.white)
-                        }
-                    }
+                    WeatherDayView(dayOfWeek: "Fri", imageName: "sun.haze.fill", temperature: 2)
                     // day 5
-                    VStack {
-                        Text("Sat")
-                            .font(.system(size: 16, weight: .medium, design: .default))
-                            .foregroundColor(.white)
-                            .padding()
-                        VStack(spacing: 5) {
-                            Image(systemName: "sun.snow.fill")
-                                .renderingMode(.original)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 60, height: 60)
-                            Text("-3°")
-                                .font(.system(size: 28, weight: .bold))
-                                .foregroundColor(.white)
-                        }
-                    }
+                    WeatherDayView(dayOfWeek: "Sat", imageName: "sun.snow.fill", temperature: -3)
                 }
+                .padding(.bottom, 50)
                 
                 VStack {
-                    Button("Change time of day") {
-                        //Text("Change time of day")
+                    Button() {
+                        print("click")
+                    } label: {
+                        Text("Change time of day")
+                            .frame(width: 250, height: 50)
+                            .background(.white)
+                            .cornerRadius(15)
+                            .font(.system(size: 20, weight: .bold, design: .default))
+                            .padding()
                     }
-                    .fontWeight(.bold)
-                    .frame(width: 250, height: 50)
-                    .foregroundColor(.blue)
-                    .background(.white)
-                    .cornerRadius(15)
-                    .padding(80)
-                    
                 }
                 Spacer()
             }
@@ -141,6 +68,29 @@ struct ContentView: View {
     ContentView()
 }
 
-func signIn() {
-    // something
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+                .padding()
+            VStack(spacing: 5) {
+                Image(systemName: imageName)
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 40, height: 40)
+                Text("\(temperature)°")
+                    .font(.system(size: 28, weight: .bold))
+                    .foregroundColor(.white)
+            }
+        }
+    }
 }
