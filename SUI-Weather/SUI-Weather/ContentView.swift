f3c1bc90b3443d7b15f67bb4e01e1e30a10853bc
Alejandro Acevedo <dogedrive@alejandrosair2.home>
2023-10-30 02:04:02 -0400

    created repo, added gitignore file

---
+++ SUI-Weather/SUI-Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SUI-Weather
+//
+//  Created by Alejandro Acevedo on 2023-10-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SUI-Weather/SUI-Weather/SUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUI_WeatherApp.swift
+//  SUI-Weather
+//
+//  Created by Alejandro Acevedo on 2023-10-30.
+//
+
+import SwiftUI
+
+@main
+struct SUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
